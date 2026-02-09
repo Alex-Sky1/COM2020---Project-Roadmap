250bcc42324bbe6a1907314769695754ffbf21a0
rb1043 <rb1043@exeter.ac.uk>
2026-02-09 02:49:05 +0000

    Fixed forms and input handling, fixed some display issues

--- Project Files/Team_Roadmap/src/main/resources/static/addItem.js
+++ Project Files/Team_Roadmap/src/main/resources/static/addItem.js
@@ -1,8 +1,13 @@
+// array to store the items in the bundle
+let items = [];
+
 // define the input text box and the place to put the items
 const inputField = document.getElementById('item');
 const listContainer = document.getElementById('items_list');
 const quantityInput = document.getElementById('quantity');
+const hiddenItems = document.getElementById('hidden_items')
 
+// adds an item to the list of items in the bundle and displays it on the screen
 function addListItem() {
 
     // get rid of whitespace
@@ -19,6 +24,13 @@ function addListItem() {
         // create a new list element with the input item
         const newListItem = document.createElement('li');
         newListItem.textContent = itemText;
+        newListItem.style.margin = "10px 0";
+
+        // add the item to the array
+        items.push(itemText);
+
+        // add the full array to the hidden input
+        hiddenItems.setAttribute('value', items.toString())
 
         // add a button to delete the item
         const deleteButton = document.createElement('button');
@@ -30,6 +42,14 @@ function addListItem() {
         // handle clicks of the delete button
         deleteButton.addEventListener('click', () => {
             listContainer.removeChild(newListItem);
+
+            // remove the item from the array
+            // if there are multiple items of the same name, it doesn't matter which one is removed
+            let index = items.indexOf(itemText);
+            // only splice array when item is found
+            if (index > -1) {
+                items.splice(index, 1);
+            }
         });
 
         // add the item and the delete button to the list
