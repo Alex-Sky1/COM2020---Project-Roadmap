8205dada1b98f2b794e3a597e59cf95b35406841
kf440 <kf440@exeter.ac.uk>
2026-03-13 10:32:20 +0000

    Add admin class and role

--- Project Files/Team_Roadmap/src/main/java/com/waste_manager/team_roadmap/WebSecurityConfig.java
+++ Project Files/Team_Roadmap/src/main/java/com/waste_manager/team_roadmap/WebSecurityConfig.java
@@ -27,9 +27,14 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        http.authorizeHttpRequests((requests)->requests.requestMatchers("/", "/sign_up_consumer", "/sign_up_seller", "/styling.css", "/images/**","/js/**", "/tos_consumer", "/tos_seller", "/privacy_policy_consumer", "/privacy_policy_seller").permitAll() //set pages which all users access
-                .requestMatchers("/dashboard_seller", "/edit_bundle_seller", "/edit_profile_seller", "/post_bundle_seller", "/manage_bundles_seller", "/manage_reservations_seller", "/view_analytics_seller", "/forecasting_seller").hasRole("SELLER") //set pages seller can access
-                .requestMatchers("/dashboard_consumer", "/edit_profile_consumer", "/view_analytics_consumer", "/manage_bundles_consumer", "/browse_bundles_consumer", "/report_issue_consumer").hasRole("CUSTOMER") //set pages customer can access
+        String[] permitAllPages = {"/", "/sign_up_consumer", "/sign_up_seller", "/styling.css", "/images/**","/js/**", "/tos_consumer", "/tos_seller", "/privacy_policy_consumer", "/privacy_policy_seller"};
+        String[] permitSeller = {"/dashboard_seller", "/edit_bundle_seller", "/edit_profile_seller", "/post_bundle_seller", "/manage_bundles_seller", "/manage_reservations_seller", "/view_analytics_seller", "/forecasting_seller", "/manage_issue_seller"};
+        String[] permitCustomer = {"/dashboard_consumer", "/edit_profile_consumer", "/view_analytics_consumer", "/manage_bundles_consumer", "/browse_bundles_consumer", "/report_issue_consumer", "/view_issue_consumer"};
+        String[] permitAdmin = {};
+        http.authorizeHttpRequests((requests)->requests.requestMatchers(permitAllPages).permitAll() //set pages which all users access
+                .requestMatchers(permitSeller).hasAnyRole("SELLER", "ADMIN") //set pages seller can access
+                .requestMatchers(permitCustomer).hasAnyRole("CUSTOMER", "ADMIN") //set pages customer can access
+                .requestMatchers(permitAdmin).hasAnyRole("ADMIN")
                 .anyRequest().authenticated()).formLogin((form)->form.loginPage("/sign_in").permitAll().successHandler(successHandler)).logout(LogoutConfigurer::permitAll); //login using /signup page and allow logout
         return http.build();
     }
