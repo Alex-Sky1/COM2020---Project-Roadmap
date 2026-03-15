8021975c0b94b184238967e5ca6631b69049a26c
Alex-Sky1 <mjd244@exeter.ac.uk>
2026-03-15 16:31:23 +0000

    Additions to Customer Controller Tests

--- Project Files/Team_Roadmap/src/test/java/com/waste_manager/team_roadmap_tests/CustomerControllerTest.java
+++ Project Files/Team_Roadmap/src/test/java/com/waste_manager/team_roadmap_tests/CustomerControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -28,4 +29,30 @@ public class CustomerControllerTest {
                 .hasStatusOk()
                 .hasViewName("sign_in");
     }
+
+    @Test
+    void testLoginSubmit() {
+        mockMvcTester.post()
+                .uri("/sign_up_consumer")
+                .param("fname", "Gordon")
+                .param("sname", "Ramsey")
+                .param("dname", "Gordon Ramsey")
+                .param("address_line_1", "26 Wren Drive")
+                .param("postcode", "B10 8SV")
+                .param("county", "West Midlands")
+                .param("email", "RamseyG32@HellsKitchen.gg")
+                .param("phone", "01967 425395")
+                .param("password1", "1diotSandw!tch")
+                .param("password2", "1diotSandw!tch")
+                .param("accept", "accept")
+                .assertThat()
+                .hasStatusOk();
+    }
+//
+//    (@RequestParam("fname") String fname, @RequestParam("sname") String sname,
+//    @RequestParam("dname") String dname, @RequestParam("address_line_1") String al1,
+//    @RequestParam("postcode") String pcode, @RequestParam("county") String county,
+//    @RequestParam("email") String email, @RequestParam("phone") String phone,
+//    @RequestParam("password1") String pwd1, @RequestParam("password2") String pwd2,
+//    @RequestParam(value = "accept", required = false) String tosAccept, Model model)
 }
